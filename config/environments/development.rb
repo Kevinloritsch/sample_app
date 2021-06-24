56aa7a85d07c7be0da169cbd081396c10ee2442d
Kevinloritsch <kloritsch58@gmail.com>
2021-06-24 09:47:38 -0700

    Add Hello

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,11 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  Rails.application.configure do
+  
+    # Allow connections to local server.
+    % config.hosts.clear
+    end
+
 end
