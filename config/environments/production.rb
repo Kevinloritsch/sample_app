c002b01fc3b86b5728070aa6fc13f710990c06ce
Kevinloritsch <kloritsch58@gmail.com>
2021-08-12 13:45:05 -0700

    Commas

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,11 +72,11 @@ Rails.application.configure do
    config.action_mailer.default_url_options = { host: host }
    ​ActionMailer::Base.smtp_settings = {
     ​:address => 'smtp.sendgrid.net'
-    ​:port           => '587',
-    ​:authentication => :plain,
-    :user_name      => 'apikey',
-    :password       => ENV['SENDGRID_API_KEY'],
-    :domain         => 'heroku.com',
+    ​:port           => '587'
+    ​:authentication => :plain
+    :user_name      => 'apikey'
+    :password       => ENV['SENDGRID_API_KEY']
+    :domain         => 'heroku.com'
     ​:enable_starttls_auto => true
   ​}
 
